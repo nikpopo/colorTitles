133d69fddd1789edfd9298e73bb47edd03ab8024
Nikita Popov <nikitapopov2002@yandex.ru>
2023-12-05 14:32:06 +0800

    final

--- app/src/main/java/com/example/colortiles/MainActivity.java
+++ app/src/main/java/com/example/colortiles/MainActivity.java
@@ -1,87 +1,50 @@
 package com.example.colortiles;
 
-import android.annotation.SuppressLint;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import com.example.colortitles.R;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Optional;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    int darkColor;
-    int brightColor;
-    ColorTileView[][] tiles = new ColorTileView[4][4];
+    ArrayList<String> names = new ArrayList<>();
+    ListView lv;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        darkColor = Color.DKGRAY;
-        brightColor = Color.LTGRAY;
+        names.add("Никита Попов");
 
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                String tileID = "t" + i + j;
-                int resID = getResources().getIdentifier(tileID, "id", getPackageName());
-                tiles[i][j] = findViewById(resID);
-                setRandomColor(tiles[i][j]);
-            }
-        }
-    }
-    private void setRandomColor(ColorTileView tile) {
-        Random random = new Random();
-        int color = random.nextBoolean() ? Color.BLUE : Color.CYAN;
-        tile.setBackgroundColor(color);
-    }
+        lv = findViewById(R.id.list);
+        adapter = new ArrayAdapter<>(this, R.layout.item, names);
+        lv.setAdapter(adapter);
 
-    public void onClick(View v) {
-        String tag = v.getTag().toString();
-        int x = Character.getNumericValue(tag.charAt(0));
-        int y = Character.getNumericValue(tag.charAt(1));
+        // Sort the initial list
+        Collections.sort(names);
 
-        toggleColor(x, y);
+        Button addButton = findViewById(R.id.addButton);
 
-        int res = checkWin();
+        addButton.setOnClickListener(view -> {
+            // Generate a random name and add it to the list
+            String[] namesArray = getResources().getStringArray(R.array.names_array);
+            String[] surnamesArray = getResources().getStringArray(R.array.surnames_array);
+            Random rand = new Random();
+            String randomName = namesArray[rand.nextInt(namesArray.length)];
+            String randomSurname = surnamesArray[rand.nextInt(surnamesArray.length)];
+            names.add(randomName + " " + randomSurname);
 
-        if (res != 0) {
-            String winningColor = String.format("#%06X", (0xFFFFFF & res));
-            if (winningColor.equals("#5DADE2")) {
-                Toast.makeText(this, "Победа синих", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(this, "Победа голубых", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    private void toggleColor(int x, int y) {
-        tiles[x][y].toggleColor();
-        for (int i = 0; i < 4; i++) {
-            if (i != x) {
-                tiles[i][y].toggleColor();
-            }
-            if (i != y) {
-                tiles[x][i].toggleColor();
-            }
-        }
-    }
-
-    private int checkWin() {
-        int firstColor = tiles[0][0].getBackgroundColor();
-
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                int currentColor = tiles[i][j].getBackgroundColor();
-                if (currentColor != firstColor) {
-                    return 0;
-                }
-            }
-        }
-        return firstColor;
+            // Notify the adapter of the change
+            adapter.notifyDataSetChanged();
+        });
     }
 }
-
